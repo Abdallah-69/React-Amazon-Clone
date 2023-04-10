97fbfcfdbeaaa80f5795e7156f5f0efca8959c6e
Abdallah Abdourahman <abdullah-clever@Abdallahs-MacBook-Air.local>
2023-04-11 00:08:49 +0800

    changing state hook to reduce hook to improve the state tracking

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -1,38 +1,72 @@
 import { Link } from 'react-router-dom';
 // import data from '../data';
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import axios from 'axios'
+import logger from 'use-reducer-logger';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_REQUEST':
+            return { ...state, loading: true };
+        case 'FETCH_SUCCESS':
+            return { ...state, products: action.payload, loading: false };
+        case 'FETCH_FAIL':
+            return { ...state, loading: false, error: action.payload };
+        default:
+            return state
+    }
+}
 function HomeScreen() {
     // API
     const API = 'http://localhost:5000'
-    const [products, setProduct] = useState([]);
+    const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+        products: [],
+        loading: true,
+        error: '',
+    });
+    // const [products, setProducts] = useState([]);
     useEffect(() => {
         const fetchData = async () => {
-            const result = await axios.get(`${API}/api/products`);
-            setProduct(result.data);
-        };
-        fetchData()
-    }, []);
-    return <div>
-        <h1> Feature Products</h1>
-        <div className="products">
-            {
-                products.map((product) => (
-                    <div className="product" key={product.slug}>
-                        <Link to={`/product/${product.slug}`}>
-                            <img src={product.image} alt={product.name} />
-                        </Link>
-                        <div className="product-info">
-                            <Link to={`/product/${product.slug}`}>
-                                <p>{product.name}</p>
-                            </Link>
-                            <p><strong>{product.price}</strong></p>
-                            <button>Add to cart</button>
-                        </div>
-                    </div>
-                ))
+            dispatch({ type: 'FETCH_REQUEST' });
+            try {
+                const result = await axios.get(`${API}/api/products`);
+                dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+            } catch (err) {
+                dispatch({ type: 'FETCH_FAIL', payload: err.message });
             }
+            // setProducts(result.data);
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+          <h1>Featured Products</h1>
+          <div className="products">
+            {loading ? (
+              <div>Loading...</div>
+            ) : error ? (
+              <div>{error}</div>
+            ) : (
+              products.map((product) => (
+                <div className="product" key={product.slug}>
+                  <Link to={`/product/${product.slug}`}>
+                    <img src={product.image} alt={product.name} />
+                  </Link>
+                  <div className="product-info">
+                    <Link to={`/product/${product.slug}`}>
+                      <p>{product.name}</p>
+                    </Link>
+                    <p>
+                      <strong>${product.price}</strong>
+                    </p>
+                    <button>Add to cart</button>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
         </div>
-    </div>
+      );
 }
 export default HomeScreen
